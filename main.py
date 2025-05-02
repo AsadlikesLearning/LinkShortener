309f3737b81947e1c450175c3c81d54e75b279f8
Asad Abdullah <asadabdullah176@gmail.com>
2025-05-03 00:00:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ def redirection(short_url):
          return f'<h1'>'Url doesnt exist</h1>'
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
